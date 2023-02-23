cb6612772982bdde44c89054f0da6ab1d4381e81
lichun <lchrennew@126.com>
2023-02-23 13:30:51 +0800

    opt: align to utc time

--- src/calculate-elapsed.js
+++ src/calculate-elapsed.js
@@ -1,9 +1,13 @@
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import duration from 'dayjs/plugin/duration.js';
 import { iso8601 } from './humanize.js';
 
+dayjs.extend(utc)
+dayjs(duration)
 
-export default (startedAt, stoppedAt = new Date(), hr = false) => {
+export default (startedAt, stoppedAt = dayjs.utc(), hr = false) => {
     if (startedAt) {
-        return iso8601(dayjs.duration(dayjs(stoppedAt).diff(dayjs(startedAt))).toISOString(), hr)
+        return iso8601(dayjs.duration(dayjs.utc(stoppedAt).diff(dayjs.utc(startedAt))).toISOString(), hr)
     }
 }
